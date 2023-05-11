b9617eedd4ed3c06e7630a920f583ddc72c54f5f
tonhuvt <132767490+tonhuvt@users.noreply.github.com>
2023-05-12 01:44:51 +0700

    2nd update

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,96 +1,57 @@
+import './styles.scss';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 const Register = () =>{
     return(
     <>
-       <h1>ĐĂNG KÝ TÀI KHOẢN</h1>
-        <p>Nếu chưa có tài khoản vui lòng đăng ký tại đây!</p>
-        <form id="register-form">
-            <div className='flex1'>
-                <label for="first-name">Họ*
-                    <br></br>
-                         <input
-                            type="text"
-                            id="first-name"
-                            name="first-name"
-                            size="50"
-                            required
-                        />
-                </label>
-                    
-                <label for="last-name">Tên*
-                    <br></br>
-                        <input
-                            type="text"
-                            id="last-name"
-                            name="last-name"
-                            size="50"
-                            required
-                        />
-                    </label>
-             </div>
-                    <label for="email">Email*
-                    <br></br>
-                            <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            size="50"
-                            required
-                            />
-                    </label>
-                    
-                    <label for="password">Mật khẩu*
-                    <br></br>
-                            <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            size="50"
-                            required
-                            />
-                    </label>
-                   
-                <div className='flex1'>
-                    <input type="submit" value="Đăng ký"/>
+        <h1>ĐĂNG KÝ TÀI KHOẢN</h1>
+        <Form>
+        
+            <Row className="mb-3">
+            <p>Nếu chưa có tài khoản vui lòng đăng ký tại đây!</p>
+            </Row>
+
+            <Row className="mb-2">
+                <Col sm={5} className="my-1">
+                    <Form.Group controlId="formBasicFirstName">
+                        <Form.Label>Họ*</Form.Label>
+                        <Form.Control type="text" placeholder="Hãy nhập họ"/>
+                    </Form.Group>
+                </Col>
+                <Col sm={5} className="my-1">
+                    <Form.Group as={Col} controlId="formBasicLastName">
+                        <Form.Label>Tên*</Form.Label>
+                        <Form.Control type="text" placeholder="Hãy nhập tên"/>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row className="mb-2">
+                <Col sm={5} className="my-1">
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email*</Form.Label>
+                        <Form.Control type="email" placeholder="Hãy nhập email"/>
+                    </Form.Group>
+                </Col>
+                <Col sm={5} className="my-1">
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Mật khẩu*</Form.Label>
+                        <Form.Control type="password" placeholder="Hãy nhập mật khẩu"/>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row className="mt-5">
+                <Col>
+                    <Button variant="primary" type="submit">Đăng ký</Button>
+                </Col>
+                <Col xs={11} className="my-2">
                     <a href="login.html">Đăng nhập</a>
-                </div>
-        </form>  
+                </Col>
+            </Row>
+        </Form>
     </>)
 }
-/*
-document.getElementById('register-form').addEventListener('submit', (event) => {
-        event.preventDefault();
-        const firstName = document.getElementById('first-name').value;
-        const lastName = document.getElementById('last-name').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        if (firstName.trim() === '' || lastName.trim() === '' || email.trim() === '' || password.trim() === '') {
-            alert('Vui lòng điền đầy đủ thông tin đăng ký');
-            return;
-        }
-        if (!validateEmail(email)) {
-            alert('Vui lòng nhập email hợp lệ');
-            return;
-        }
-        if (!validatePassword(password)) {
-            alert('Mật khẩu phải có ít nhất 8 ký tự');
-            return;
-        }
-        
-        register(firstName, lastName, email, password);
-    });
-
-    function validateEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-
-    function validatePassword(password) {
-        return password.length >= 8;
-    }
-
-    function register(firstName, lastName, email, password) {
-        
-        alert('Bạn đã đăng ký thành công');
-    }
-*/
 export default Register;
